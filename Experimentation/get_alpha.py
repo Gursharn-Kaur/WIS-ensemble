b9af0e8ade251df872fe130860879000d181116e
Nibir088 <nibir338@gmail.com>
2023-08-28 22:51:47 -0400

    get_alpha.py

---
+++ Experimentation/get_alpha.py
@@ -0,0 +1,19 @@
+'''
+    read a text file containing all quantiles.
+    
+    takes a argument-- fname.
+    
+    fname: str, default='alpha.txt'. file name (location) of the quantiles.
+    
+    Returns-
+    
+    alphas: containing all quantiles.
+'''
+
+
+def get_alpha(fname='alpha.txt'):
+    f_alpha = open(fname,'r')
+    read_alpha = f_alpha.readlines()[0].split(' ')
+    alphas = [float(a) for a in read_alpha]
+    return alphas
+
